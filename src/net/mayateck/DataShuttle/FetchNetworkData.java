db0155e1a528f07ee811cf1c6743d969066e9689
Matthew Struble <Matt@MayateckInnovations.net>
2013-07-05 20:13:03 -0700

    Added ping for initialization.

--- src/net/mayateck/DataShuttle/FetchNetworkData.java
+++ src/net/mayateck/DataShuttle/FetchNetworkData.java
@@ -1,14 +1,31 @@
 package net.mayateck.DataShuttle;
 
+import java.net.*;
+import java.io.*;
+
+//@SuppressWarnings("unused")
 public class FetchNetworkData {
 
 	public static boolean checkConnectionToServer(){
-		boolean conn = false;
+		boolean connected = false;
+		try {
+			URL url = new URL("http://services.mayateck.net/datashuttle/dataHandler.php?type=ping");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			BufferedReader read = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line;
+			String result="";
+			while ((line = read.readLine()) != null) {
+				result += line;
+			}
+			if (result=="connected"){
+				connected=true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
-		// TODO: Fetch database script on web server!
-			// http://services.mayateck.net/datashuttle/dataHandler.php
-		
-		return conn;
+		return connected;
 	}
 	
 	public static int fetchPlayerData(String pName){
